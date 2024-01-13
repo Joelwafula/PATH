4b9819326f6363e8e9713c4abf53272571f3e906
Joel <joelwafula2020@gmail.com>
2024-01-14 00:10:16 +0300

    "breaking handles into functions"

--- main.go
+++ main.go
@@ -3,26 +3,43 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header.Set("Content-Type", "text/html")
+// func handlerFunc(w http.ResponseWriter, r *http.Request) {
+// 	w.Header.Set("Content-Type", "text/html")
 
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>welcome to this site</h1>")
+// 	if r.URL.Path == "/" {
+// 		fmt.Fprint(w, "<h1>welcome to this site</h1>")
 
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "to get in touch please send an email"+
-			"to <a href=\"mailto:support@lenslocked.com\">"+
-			"support@lenslocked.com</a>.")
+// 	} else if r.URL.Path == "/contact" {
+// 		fmt.Fprint(w, "to get in touch please send an email"+
+// 			"to <a href=\"mailto:support@lenslocked.com\">"+
+// 			"support@lenslocked.com</a>.")
 
-	} else {
-		w.WriteHeader(http.StatusNotFound)
+// 	} else {
+// 		w.WriteHeader(http.StatusNotFound)
 
-		fmt.Fprint(w, "<h1>We could not find the page you "+
-			"were looking for :(</h1>"+
-			"<p>Please email us if you keep being sent to an "+
-			"invalid page.</p>")
-	}
+// 		fmt.Fprint(w, "<h1>We could not find the page you "+
+// 			"were looking for :(</h1>"+
+// 			"<p>Please email us if you keep being sent to an "+
+// 			"invalid page.</p>")
+// 	}
 
+// }
+
+//lets break the above into functions
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	fmt.Fprint(w, "<h1>welcome to our home page</h1>")
+
+}
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "to get in touch please send an email"+
+		"to <a href=\"mailto:support@lenslocked.com\">"+
+		"support@lenslocked.com</a>.")
 }
